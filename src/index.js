6f7cb9d422f82f5a97c35cea7215fe8259b2f9d6
Your Name <hsynozcn009@gmail.com>
2021-10-22 15:00:55 +0300

    Jsx ve html arasindaki farklar

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-/*
-function App(){
-    return <h1>My name is Huseyin </h1>
-}
-*/
-class App2 extends React.Component{
-    render(){
-        return <h1>My name is Huseyin </h1>
-    }
-}
+function App() {
+    // ? JSX ile html arasındaki farklar 
+    // *1. Tek bir ana div icerisinde kullaniliyor .
+    // !2. Style ozellikleri yaparken key value sekli yazim
+    // *3.JSX icerisine js kodları entegre edebiliyoruz.
+    // !4.HTML attributeleri icin camelCase yazim sekli kullaniliyor.
 
+    // ?
+    const str1 = "Click ";
+    const str2 = "Me";
+
+    return (
+    <div>
+        <button type="button" style={{padding:'10px', color: 'white', backgroundColor: 'blue', border: '2px solid yellow'}}>{str1.concat(str2)}</button>  
+        {/* <button type="button" style={{padding:'10px', color: 'white', backgroundColor: 'blue', border: '2px solid yellow'}}>JSX button</button> */}
+    </div>
+    );
+}
 ReactDOM.render(
-    <App2 />,
-    document.querySelector("#root")
-    //document.getElementById("root")
+    <App />,
+    document.getElementById("root")
 );

fe07c3d0d2dc90bfc20a14350b9d85b9213914f4
Your Name <hsynozcn009@gmail.com>
2021-10-25 11:08:40 +0300

    child ve multiple props

--- src/index.js
+++ src/index.js
@@ -2,17 +2,31 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 import Card from './components/card.js';
+import Collapse from './components/collapse.js';
 
 const App = () => {
-       return(
-        <div>
-            <div className="card-group">
-                <Card cardTitle="Card Title 1"/>
-                <Card cardTitle="Card Title 2"/>
-                <Card cardTitle="Card Title 3"/>
-            </div>
-        </div>
-       );
+    return (
+        <div className="card-group">
+            <Collapse href="collapseExample1">
+                Pure Collapse Content NO CARD
+            </Collapse>
+            
+            <Collapse href="collapseExample2">
+                <Card
+                        cardTitle="Card Title II"
+                        cardText="Lorem Ipsum Text II"
+                        updatedTime="Last Updated 2 min ago"
+                        image="https://picsum.photos/id/10/200/300"
+                />
+            </Collapse>
+                <Card
+                    cardTitle="Card Title III"
+                    cardText="Lorem Ipsum Text III"
+                    updatedTime="Last Updated 3 min ago"
+                    image="https://picsum.photos/id/100/200/300"
+                />
+        </div>        
+    );
 }
 
 ReactDOM.render(
